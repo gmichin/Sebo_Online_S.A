ffe8dacb8864f8a83e2bf2f60aa71e66fa2c2819
Gustavo <gmassamichi@gmail.com>
2023-10-07 03:16:51 -0300

    1 entrega finalizada- AJUSTAR O FRONT END

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/path', require('./routes/users')); 
+app.use('/path', require('./routes/path')); 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
